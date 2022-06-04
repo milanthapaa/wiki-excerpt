640d80221da9ed1c8724e946e8eb7c5468535776
Milan Thapa <milanthapa299@gmail.com>
2022-06-04 12:13:39 +0545

    retrive and create a file with excerpt data

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ def random_wiki():
     request = requests.get(
         f'https://en.wikipedia.org/api/rest_v1/page/summary/{userinput}')
     status_code = 200
+    data = request.json()
 
+    if request.status_code == status_code:
+        with open(f'{userinput}.txt', 'w') as f:
+            f.write(data['extract'])
+    else:
+        return "Couldn't retrieve the data, request error"
 
 
 if __name__ == "__main__":
