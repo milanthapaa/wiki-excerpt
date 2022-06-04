a0b9d315717ff031598415fc555012aba1f4f3a3
Milan Thapa <milanthapa299@gmail.com>
2022-06-04 12:12:36 +0545

    request data as per user

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import requests
 
 def random_wiki():
     userinput = input("Search excerpt in wikipedia: ")
+    request = requests.get(
+        f'https://en.wikipedia.org/api/rest_v1/page/summary/{userinput}')
+    status_code = 200
+
 
 
 if __name__ == "__main__":
